5471de642c1d253d2cee1c01290f592ecf7edbda
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-07-23 18:48:55 +0530

    Add EvenForLoop class to print even numbers

---
+++ EvenNumberForLoop.java
@@ -0,0 +1,14 @@
+package OOPSC26CS070;
+
+public class EvenForLoop {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Even numbers from 1 to 100:");
+
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.print(i + " ");
+        }
+	}
+
+}
