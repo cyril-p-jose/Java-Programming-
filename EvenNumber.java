0fc6850b2ff9e118fd0250b2817c9f009016a86e
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-07-28 21:56:48 +0530

    Implement do-while loop for printing even numbers
    Added a do-while loop to print even numbers from 2 to 20.

--- EvenNumber.java
+++ EvenNumber.java
@@ -12,3 +12,16 @@ public class EvenForLoop {
 	}
 
 }
+
+
+/* do-while loop */
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int i = 2;
+
+        do {
+            System.out.print(i + " ");
+            i += 2;
+        } while (i <= 20);
+    }
+}
